829cdcd97d4f37fb98d98cb4507a2d3c74744e41
iepoch <steaclem@gmail.com>
2019-06-19 20:37:41 -0700

    Added a Linked List to Que. Added some to Double Linked list

---
+++ queue/node.py
@@ -0,0 +1,42 @@
+class Node:
+
+    def __init__(self, value, next_node):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+
+    def add_to_head(self, node):
+        if self.head == None:
+            self.head = node
+            self.tail = node
+        else:
+            node.set_next(self.head)
+            self.head = node
+
+    def remove_from_head(self):
+        val = None
+        if self.head == None:
+            val = None
+        elif self.head == self.tail:
+            val = self.head.get_value()
+            self.head = None
+            self.tail = None
+        else:
+            val = self.head.get_value()
+            self.head = self.head.get_next()
+        return val
