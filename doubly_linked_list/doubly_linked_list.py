cfdbbc2107ac0530607b7e5c1e978fd4b3dfb04d
Stefan <steaclem@live.com>
2019-06-22 08:38:50 -0700

    fixing the delete

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -100,7 +100,21 @@ class DoublyLinkedList:
 
 
     def delete(self, node):
-           return node.delete()
+        self.length -=1
+        if not self.head and not self.tail:
+            return
+        elif self.head == self.tail:
+            self.head= None
+            self.tail = None
+        elif self.head == node:
+            self.head = node.next
+            node.delete()
+        elif self.tail == node:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+            
             
     def get_max(self):
         cur_max = self.head
